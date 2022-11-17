68fa43fb8a0bf5d78a809a478747b564c7b10978
David Yao <79487734+davidwyao@users.noreply.github.com>
2022-11-17 18:27:40 -0500

    Local json parsing

--- src/park-d-front-end/src/Map.js
+++ src/park-d-front-end/src/Map.js
@@ -2,23 +2,27 @@ import React, { useState } from 'react';
 import './Map.css';
 
 function ParseJSON() {
+    // import json
     const spaceData = require('./SpaceData.json');
-    console.log(spaceData.parking_spaces);
 
-    
+    const states = new Array(spaceData.parking_spaces.length);
+    for (var i = 0; i < states.length; i++) {
+        states[i] = spaceData.parking_spaces[i].status;
+    }
+    return states;
 }
 
 function Map() {
-    ParseJSON();
+    const states = ParseJSON();
     return (
         <div>
             <div class="parkingRow">
-                <div class="spaceEmpty"></div>
-                <div class="spaceFull"></div>
+                <div class = {states[1] ? "space empty" : "space full"} ></div>
+                <div class = {states[0] ? "space empty" : "space full"} ></div>
             </div>
             <div class="parkingRow">
-                <div class="spaceEmpty"></div>
-                <div class="spaceFull"></div>
+                <div class = {states[2] ? "space empty" : "space full"} ></div>
+                <div class = {states[3] ? "space empty" : "space full"} ></div>
             </div>
         </div>
     );
