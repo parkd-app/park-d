021c05ef1dccd5da72f708c17b2e537dfdc786e1
Gary Gong <gary@DESKTOP-OK4FK12.localdomain>
2022-11-18 16:44:37 -0500

    set up server

--- src/pard-d-api/app.py
+++ src/pard-d-api/app.py
@@ -1,11 +1,47 @@
 from flask import Flask
-
+from flask import request
+import logging
+import json
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+
+path = 'C:\\Users\\Gary Gong\\Desktop\\ML_model\\ParkingLot\\parking_lot\\parking_info.txt'
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!!'
+@app.route('/rt_parking_info', methods=['GET'])
+def requires_parking_spot():  # put application's code here
+    with open(path, 'r') as f:
+        parking_info = f.read()
+    app.logger.info(parking_info)
+
+    pro = str(parking_info).strip('[')[:-1]
+
+    val = []
+    ret = []
+    for i in pro.split(','):
+        print(i.strip())
+        if i == 'True':
+            val.append(True)
+
+        else:
+            val.append(False)
+    for i, ele in enumerate(val):
+        ret.append({
+            'id': i + 1,
+            'status' : ele
+        })
+
+    return {
+        'parking_spaces': ret
+    }
+
+
+@app.route('/req_coordinate', methods=['GET'])
+def requires_coordinate():
+    pass
+
+
+
 
 
 if __name__ == '__main__':
