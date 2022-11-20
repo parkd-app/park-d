9a99a4c5abd530910c15d7c9ed81c36c9dd2a15a
David Yao <79487734+davidwyao@users.noreply.github.com>
2022-11-19 23:30:51 -0500

    Display icons on parking spaces
    Close #33

--- src/park-d-front-end/src/Map.js
+++ src/park-d-front-end/src/Map.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faXmarkCircle, faWheelchair } from '@fortawesome/free-solid-svg-icons';
+
 import './Map.css';
 
 function ParseJSON() {
@@ -17,12 +20,33 @@ function Map() {
     return (
         <div>
             <div class="parkingRow">
-                <div class = {states[1] ? "space empty" : "space full"} ></div>
-                <div class = {states[0] ? "space empty" : "space full"} ></div>
+                <div class = {states[1] ? "space empty" : "space full"} >
+                    <div class = 'icon'>
+                        {states[1] ? <FontAwesomeIcon icon={faCheckCircle} color="green" size="3x" /> :
+                        <FontAwesomeIcon icon={faXmarkCircle} color="red" size="3x" />}
+                    </div>
+                </div>
+
+                <div class = {states[0] ? "space empty" : "space full"} >
+                    <div class = 'icon'>
+                        {states[0] ? <FontAwesomeIcon icon={faCheckCircle} color="green" size="3x" /> :
+                        <FontAwesomeIcon icon={faXmarkCircle} color="red" size="3x" />}
+                    </div>
+                </div>
             </div>
             <div class="parkingRow">
-                <div class = {states[2] ? "space empty" : "space full"} ></div>
-                <div class = {states[3] ? "space empty" : "space full"} ></div>
+                <div class = {states[2] ? "space empty" : "space full"} >
+                    <div class = 'icon'>
+                        {states[2] ? <FontAwesomeIcon icon={faCheckCircle} color="green" size="3x" /> :
+                        <FontAwesomeIcon icon={faXmarkCircle} color="red" size="3x" />}
+                    </div>
+                </div>
+                <div class = {states[3] ? "space empty" : "space full"} >
+                    <div class = 'icon'>
+                        {states[3] ? <FontAwesomeIcon icon={faCheckCircle} color="green" size="3x" /> :
+                        <FontAwesomeIcon icon={faXmarkCircle} color="red" size="3x" />}
+                    </div>
+                </div>
             </div>
         </div>
     );
