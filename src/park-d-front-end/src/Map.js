d764d72a036a03cfa255cb365e5826a924ff99db
David Yao <79487734+davidwyao@users.noreply.github.com>
2022-11-20 03:15:35 -0500

    Update parking lots from online json

--- src/park-d-front-end/src/Map.js
+++ src/park-d-front-end/src/Map.js
@@ -1,48 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle, faXmarkCircle, faWheelchair } from '@fortawesome/free-solid-svg-icons';
 
 import './Map.css';
 
-function ParseJSON() {
-    // import json
-    const spaceData = require('./SpaceData.json');
-
-    const states = new Array(spaceData.parking_spaces.length);
-    for (var i = 0; i < states.length; i++) {
-        states[i] = spaceData.parking_spaces[i].status;
-    }
-    return states;
-}
+const jsonURL = 'http://localhost:8000/parking_spaces'
 
 function Map() {
-    const states = ParseJSON();
+    const [states, setState] = useState([true,true,true,true]);
+    
+    useEffect(() => {
+        let interval = setInterval(() => {
+            let json_obj = JSON.parse(Get(jsonURL));
+            let statuses = json_obj.map(space => space.status)
+            setState(statuses);
+            console.log(states)
+        }, 2000);
+        return () => {
+            clearInterval(interval);
+        }
+      });
+
+    return drawSpaces(states)
+}
+
+function Get(jsonURL){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",jsonURL,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+
+function drawSpaces(states) {
     return (
         <div>
-            <div class="parkingRow">
-                <div class = {states[1] ? "space empty" : "space full"} >
-                    <div class = 'icon'>
+            <div className="parkingRow">
+                <div className = {states[1] ? "space empty" : "space full"} >
+                    <div className = 'icon'>
                         {states[1] ? <FontAwesomeIcon icon={faCheckCircle} color="green" size="3x" /> :
                         <FontAwesomeIcon icon={faXmarkCircle} color="red" size="3x" />}
                     </div>
                 </div>
 
-                <div class = {states[0] ? "space empty" : "space full"} >
-                    <div class = 'icon'>
+                <div className = {states[0] ? "space empty" : "space full"} >
+                    <div className = 'icon'>
                         {states[0] ? <FontAwesomeIcon icon={faCheckCircle} color="green" size="3x" /> :
                         <FontAwesomeIcon icon={faXmarkCircle} color="red" size="3x" />}
                     </div>
                 </div>
             </div>
-            <div class="parkingRow">
-                <div class = {states[2] ? "space empty" : "space full"} >
-                    <div class = 'icon'>
+            <div className="parkingRow">
+                <div className = {states[2] ? "space empty" : "space full"} >
+                    <div className = 'icon'>
                         {states[2] ? <FontAwesomeIcon icon={faCheckCircle} color="green" size="3x" /> :
                         <FontAwesomeIcon icon={faXmarkCircle} color="red" size="3x" />}
                     </div>
                 </div>
-                <div class = {states[3] ? "space empty" : "space full"} >
-                    <div class = 'icon'>
+                <div className = {states[3] ? "space empty" : "space full"} >
+                    <div className = 'icon'>
                         {states[3] ? <FontAwesomeIcon icon={faCheckCircle} color="green" size="3x" /> :
                         <FontAwesomeIcon icon={faXmarkCircle} color="red" size="3x" />}
                     </div>
