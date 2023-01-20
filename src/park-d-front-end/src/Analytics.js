7acfa32a2c754b06101d71d583fb8b1c1ba5b6a0
Kabishan <ksuvendran@gmail.com>
2023-01-19 22:05:58 -0500

    Rebased previous branch to include main; added graph

--- src/park-d-front-end/src/Analytics.js
+++ src/park-d-front-end/src/Analytics.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Analytics.css';
 
+import { BarChart, Bar, XAxis, Tooltip, CartesianGrid } from 'recharts';
+
 // set url to fetch parking data
 const jsonURL = 'http://localhost:8000/parking_spaces'
 
@@ -12,6 +14,34 @@ function Get(yourUrl){
 }
 
 function Analytics() {
+    const data = [
+        { name: "12am", occupancy: 1 },
+        { name: "1am", occupancy: 1 },
+        { name: "2am", occupancy: 1 },
+        { name: "3am", occupancy: 1 },
+        { name: "4am", occupancy: 1 },
+        { name: "5am", occupancy: 1 },
+        { name: "6am", occupancy: 2 },
+        { name: "7am", occupancy: 3 },
+        { name: "8am", occupancy: 4 },
+        { name: "9am", occupancy: 5 },
+        { name: "10am", occupancy: 8 },
+        { name: "11am", occupancy: 9 },
+        { name: "12pm", occupancy: 10 },
+        { name: "1pm", occupancy: 9 },
+        { name: "2pm", occupancy: 8 },
+        { name: "3pm", occupancy: 3 },
+        { name: "4pm", occupancy: 6 },
+        { name: "5pm", occupancy: 7 },
+        { name: "6pm", occupancy: 8 },
+        { name: "7pm", occupancy: 9 },
+        { name: "8pm", occupancy: 6 },
+        { name: "9pm", occupancy: 3 },
+        { name: "10pm", occupancy: 2 },
+        { name: "11pm", occupancy: 1 },
+    ];
+
+    // The following code will be used to retrieve live occupancy data
     //spotNum[0] = # of available spots, spotNum[1] = # of occupied spots
     const [spotNum, setSpotNum] = useState([0,0]);
 
@@ -37,12 +67,37 @@ function Analytics() {
 
     return (
         <div>
-            <div><h2>Analytics</h2></div>
-            <div>
-                {"Available spots: " + spotNum[0]}
-            </div>
-            <div>
-                {"Occupied spots: " + spotNum[1]}
+            { /*
+                <div>
+                    {"Available spots: " + spotNum[0]}
+                </div>
+                <div>
+                    {"Occupied spots: " + spotNum[1]}
+                </div>
+            */ }
+
+            { /* The following code will be used to retrieve historical occupancy data */ }
+
+            <h2 style={{ textAlign: "center" }}>Analytics</h2>
+            <div className="App">
+                <BarChart width={500} height={300} data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 80,
+                    bottom: 5,
+                }}
+                barSize={20}>
+                <XAxis
+                    dataKey="name"
+                    scale="point"
+                    padding={{ left: 10, right: 10 }}
+                />
+                <Tooltip />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Bar dataKey="occupancy" fill="#8884d8" background={
+                    { fill: "#eee" }} />
+                </BarChart>
             </div>
         </div>
     );
