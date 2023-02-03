a1968f4a50b8ae08cc1a6d44372d7d73d29c978f
= <albertzhou132@gmail.com>
2023-02-02 22:38:28 -0500

    Directions from demo

--- src/park-d-front-end/src/HomeView.js
+++ src/park-d-front-end/src/HomeView.js
@@ -10,13 +10,12 @@ var noPoi = [
 var defaultOptions = {
     zoom:15,
     center:{lat:43.2609,lng:-79.9192},
-    styles: noPoi
+    disableDefaultUI: true,
+    styles: noPoi,
   }
-var mapBounds;
 
 var directionsRenderer;
 var directionsService;
-
 var clickOrigin;
 var clickDestination;
 var clickChoice = 0;
@@ -29,24 +28,80 @@ function initMap(){
     directionsService = new google.maps.DirectionsService();
 
     map = new google.maps.Map(document.getElementById('map'), defaultOptions);
-    mapBounds = new google.maps.LatLngBounds();
 
     google.maps.event.addListener(map, 'click', function(event){
         if (clickChoice == 0)
         {
             clickOrigin = {coords:event.latLng};
-            routeMarkers.push(placeMarker(clickOrigin.coords, "./CarMarker.png"));
+            routeMarkers.push(placeMarker(clickOrigin.coords, "./Images/CarMarker.png"));
               pickDestination();
         }
         else if (clickChoice == 1)
         {
             clickDestination = {coords:event.latLng};
             directionsRenderer.setMap(map);
-            getRoute(directionsRenderer, directionsService);
+            getRoute();
             pickDone();
         }
     });
 
+}
+
+function pickDone()
+{
+    clickChoice = -1;
+    getDocEle("direction_guide").textContent = "Route Calculated";
+}
+
+function pickDestination()
+{
+    clickChoice = 1;
+    getDocEle("direction_guide").textContent = "Select Destination";
+}
+
+function placeMarker(position, icon)
+{
+    marker = new google.maps.Marker({
+        position: position,
+        map:map,
+        icon: icon,
+      });
+    return marker;
+}
+
+function resetData()
+{
+    clickChoice = 0;
+    getDocEle("direction_guide").textContent = "Select your location";
+    clearMarkers(routeMarkers);
+    routeMarkers = [];
+    directionsRenderer.setMap(null);
+}
+
+function clearMarkers(markers)
+{
+    for (let index = 0; index < markers.length; index++) {
+        markers[index].setMap(null);
+        markers[index] = null;
+    }
+}
+
+function getRoute()
+{
+    directionsService
+    .route({
+            origin:clickOrigin.coords,
+            destination:clickDestination.coords,
+            travelMode:google.maps.TravelMode["DRIVING"],
+    })
+    .then((response) => {
+        directionsRenderer.setDirections(response);
+        clearMarkers(routeMarkers);
+        routeMarkers = [];
+        routeMarkers.push(placeMarker(response.routes[0].legs[0].start_location, "./Images/CarMarker.png"));
+        routeMarkers.push(placeMarker(response.routes[0].legs[0].end_location, "./Images/ParkingMarker.png"));
+    })
+    .catch((e) => window.alert("Direction request failed."));
 }
 
 function toggleSelection()
