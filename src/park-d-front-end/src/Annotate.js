55e018ccba5afbbb3332de7ea6fc36e6bbe8fca4
Jonathan Arta Yapeter <jonathanarta.yapeter@yahoo.com>
2023-02-05 02:36:13 -0500

    deletes annotations from json server

--- src/park-d-front-end/src/Annotate.js
+++ src/park-d-front-end/src/Annotate.js
@@ -1,44 +1,48 @@
-function appendAnnotation(annotation) {
-  renderAnnotations();
+// async function fetchAnnotations() {
+//   try {
+//     const response = await fetch("http://localhost:8000/annotations", {
+//       method: "GET",
+//       credentials: "same-origin",
+//     });
+//     const obj = await response.json();
+//     return obj;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// async function renderAnnotations() {
+//   const obj = await fetchAnnotations();
+//   console.log(obj);
+//   let str = JSON.stringify(obj);
+//   console.log(str);
+//   return str;
+// }
+
+function writeToJSON(annotation, currAnnotations) {
+  newId = currAnnotations.length - 2;
+  annotation.id = newId;
+  fetch("http://localhost:8000/annotations", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(annotation),
+  })
+    .then((response) => response.json())
+    .then((response) => console.log(JSON.stringify(response)));
 }
 
-async function fetchAnnotations() {
-  try {
-    const response = await fetch("./annotations.w3c.json", {
-      method: "GET",
-      credentials: "same-origin",
-    });
-    const exam = await response.json();
-    return exam;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function renderAnnotations() {
-  const exam = await fetchAnnotations();
-  console.log(exam);
-  let str = JSON.stringify(exam);
-  console.log(str);
-  return str;
-}
-
-async function writeToJSON() {
-  // create a JSON object
-  const user = {
-    id: 1,
-    name: "John Doe",
-    age: 22,
-  };
-
-  // convert JSON object to a string
-  const data = JSON.stringify(user);
-
-  // write JSON string to a file
-  fs.writeFile("user.json", data, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("JSON data is saved.");
-  });
+async function deleteAnnotation(annotation) {
+  fetch("http://localhost:8000/annotations/" + annotation.id, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(annotation),
+  })
+    .then((response) => response.json())
+    .then((response) => console.log(JSON.stringify(response)));
 }
