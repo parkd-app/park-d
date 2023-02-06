61b85779c23170140976d7649996dc58410d8a3c
Jonathan Arta Yapeter <jonathanarta.yapeter@yahoo.com>
2023-02-06 03:02:13 -0500

    w3c to custom json format

--- src/park-d-front-end/src/Annotate.js
+++ src/park-d-front-end/src/Annotate.js
@@ -1,6 +1,12 @@
 var view = "bird";
 
 function writeToJSON(annotation, currAnnotations) {
+  if (view == "bird") {
+    w3cToBird(annotation);
+  } else {
+    w3cToSide(annotation);
+  }
+
   newId = currAnnotations.length - 2;
   annotation.id = newId;
   fetch("http://localhost:8000/annotations_" + view, {
@@ -47,4 +53,20 @@ function getView() {
 
 function setView(newView) {
   view = newView;
+}
+
+function w3cToBird(annotation) {
+  var annStr = JSON.stringify(annotation.target.selector.value);
+  annStr = annStr.slice(12, annStr.length - 1);
+  annArr = annStr.split(",");
+  console.log("annStr = " + annStr);
+  console.log(annArr);
+}
+
+function w3cToSide(annotation) {
+  var annStr = JSON.stringify(annotation.target.selector.value);
+  annStr = annStr.slice(24, annStr.length - 20);
+  annArr = annStr.split(" ");
+  console.log("annStr = " + annStr);
+  console.log(annArr);
 }
