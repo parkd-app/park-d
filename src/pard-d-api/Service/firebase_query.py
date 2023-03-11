387bb1f540c890beba637d90a3ea57c54dfc39c2
gary <g754313990@outlook.com>
2023-03-11 14:16:05 -0500

    add firebase

---
+++ src/pard-d-api/Service/firebase_query.py
@@ -0,0 +1,28 @@
+import firebase_admin
+import os
+from firebase_admin import credentials
+from firebase_admin import db
+
+
+def start_db():
+    current_dir = os.getcwd()
+    parent_dir = os.path.dirname(current_dir)
+    credential_path = "park-d-dev-firebase-adminsdk-m7q98-c97bc3fdfc.json"
+
+    cred = credentials.Certificate(credential_path)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://park-d-dev-default-rtdb.firebaseio.com/'
+    })
+
+def query_by_key(key):
+    ref = db.reference()
+    query = ref.order_by_key().equal_to(key)
+    return query.get()[key]
+
+
+def set_by_key(key,value):
+    ref = db.reference()
+    ret = ref.set({key:value})
+    return  ret
+
+
