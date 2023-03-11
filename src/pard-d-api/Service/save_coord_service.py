1463814edd3e39215a4f59e14406ab0d0e69f6e4
gary <g754313990@outlook.com>
2023-03-11 15:33:34 -0500

    add databse query

--- src/pard-d-api/Service/save_coord_service.py
+++ src/pard-d-api/Service/save_coord_service.py
@@ -1,5 +1,8 @@
 import pickle
 
+from Service.firebase_query import set_by_key,query_by_key
+
+
 def save_coordinates(data, angle):
     with open(
             "C:\\Users\\g7543\\OneDrive\\桌面\\Capstone_project"
@@ -7,3 +10,25 @@ def save_coordinates(data, angle):
     ) as f:
         pickle.dump(data, f)
     return 1
+
+
+def create_unique_key(id,name):
+    return name + "_parking_" + str(id) + "_info"
+
+def get_existing_layout(id,name):
+    return query_by_key(create_unique_key(id,name))
+def save_map_coordinate(data):
+    parking_spec = data["parking_lots"]
+    name = parking_spec["owner"]
+    id = parking_spec["id"]
+    set_by_key(create_unique_key(id,name), data)
+    return 200
+
+
+
+
+
+
+
+
+
