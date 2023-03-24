cd11ee92d9f2e0eb7fd8075b56d937f979464565
unknown <g754313990@outlook.com>
2023-03-24 12:48:05 -0400

    add snapshot

--- src/pard-d-api/Service/snap_shot_service.py
+++ src/pard-d-api/Service/snap_shot_service.py
@@ -1,28 +1,20 @@
-import cv2
-import os
-import logging
+from io import BytesIO
+
+from pytube import YouTube
+from PIL import Image
+import requests
+
+def save_frame_sec(url):
+    yt = YouTube(url)
+    thumbnail_url = yt.thumbnail_url
+    # Read the thumbnail image from URL using OpenCV
+    im = Image.open(requests.get(thumbnail_url, stream=True).raw)
+    return serve_pil_image(im)
+
+def serve_pil_image(pil_img):
+    img_io = BytesIO()
+    pil_img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return img_io
 
 
-def save_frame_sec(video_path, sec, result_path):
-    logger = logging.getLogger('werkzeug')
-    handler = logging.FileHandler('access.log')
-    logger.addHandler(handler)
-    cap = cv2.VideoCapture(video_path)
-    logging.info("not opend")
-
-    if not cap.isOpened():
-        logging.info("not opend")
-        return
-
-    os.makedirs(os.path.dirname(result_path), exist_ok=True)
-
-    fps = cap.get(cv2.CAP_PROP_FPS)
-
-    cap.set(cv2.CAP_PROP_POS_FRAMES, round(fps * sec))
-
-    ret, frame = cap.read()
-    result_path = result_path + '\\bird.png'
-    if ret:
-        logging.info("write to path")
-        cv2.imwrite(result_path, frame)
-    return True
