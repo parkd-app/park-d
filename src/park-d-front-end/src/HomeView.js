fcc97d78d1d2a9541aeb58ac8497549fae4be553
David Yao <79487734+davidwyao@users.noreply.github.com>
2023-03-26 16:44:22 -0400

    Add analytics reload skeleton

--- src/park-d-front-end/src/HomeView.js
+++ src/park-d-front-end/src/HomeView.js
@@ -532,6 +532,7 @@ function loadAllSpots(ID, owner) {
     window["spot" + spotData[i].id].setMap(map);
     parkingLayoutIds.push("spot" + spotData[i].id);
   }
+  reloadAnalytics();
   numSpots = spotData.length;
   clearInterval(intervalID);
   intervalID = setInterval(updateSpots, updateInterval);
@@ -683,4 +684,10 @@ function drawGraph(labels, data, backgroundColours, totalSpots) {
       },
     },
   });
+}
+
+function reloadAnalytics() {
+  // lotID and lotOwner are global variables
+  let theID = lotID;
+  let theOwner = lotOwner; 
 }
