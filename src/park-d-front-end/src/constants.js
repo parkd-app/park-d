589fd2a619049164761025debb53dc11724e5374
David Yao <79487734+davidwyao@users.noreply.github.com>
2023-04-05 01:11:33 -0400

    Cleanup, comments

--- src/park-d-front-end/src/constants.js
+++ src/park-d-front-end/src/constants.js
@@ -1,21 +1,19 @@
-// json parsing
+// other urls extend off this
 const backendURL = "https://back-end-new-api.azurewebsites.net/";
-//const backendURL = "http://localhost:8000/";
-const jsonURL = backendURL + "rt_parking_info";
-//const jsonURL = backendURL + "parking_lots";
-const lotsURL = backendURL + "get_all_parking_lots";
-//const lotsURL = "http://localhost:8001/parking_lots";
-const postURL = backendURL + "save_coord";
-//const postURL = backendURL + "parking_lots";
-const createLotURL = backendURL + "create_parking_lot";
-const allLotURL = backendURL + "get_all_parking_lots";
+const jsonURL = backendURL + "rt_parking_info"; // all spots for given lot
+const postURL = backendURL + "save_coord"; // save changed spots
+const createLotURL = backendURL + "create_parking_lot"; // create a lot in db
+const allLotURL = backendURL + "get_all_parking_lots"; // all lots
 
+// for camera annotations
 const prevLayoutURL = backendURL + "get_prev_layout";
 const snapshotURL = backendURL + "get_parking_snapshot?name=";
 const snapshotBackupURL = backendURL + "get_parking_snapshot_backup";
 
-const updateInterval = 5000;
+const updateInterval = 5000; // how often to query the backend
 
+// green: regular, blue: accessible, red: reserved
 const colorToType = { "#00FF00": 0, "#0000FF": 1, "#FF0000": 2 };
 const typeToColor = { 0: "#00FF00", 1: "#0000FF", 2: "#FF0000" };
+// grey
 const newSpotColor = "#888888";
