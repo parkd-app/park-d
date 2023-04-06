fb1affade27f160edac6949d731372ab2bab0531
Almen Ng <almen.km.ng@gmail.com>
2023-04-05 23:19:45 -0400

    make pretty
    Signed-off-by: Almen Ng <almen.km.ng@gmail.com>

--- src/park-d-front-end/src/cypress/e2e/User_Test_Suite.cy.js
+++ src/park-d-front-end/src/cypress/e2e/User_Test_Suite.cy.js
@@ -1,23 +1,23 @@
-describe('User Test Suite', () => {
-    beforeEach (() => {
-        cy.visit('index.html')
-        cy.get('#usermode').click();
-    });
-  
-    it('Proper headings and navbar', () => {
-      cy.visit('index.html')
-      cy.get('#email').click().focused().type('test@test.com');
-      cy.get('#password').click().focused().type('test1234');
-      cy.get('#submit').click();
-      cy.get('#header').should('exist');
-      cy.get('#map').should('exist');
-      cy.get('button[class="navbar-icon"]').click()
-      cy.get('#header').should('exist');
-      cy.get('#home').should('exist');
-      cy.get('#edit').should('exist');
-      cy.get('#contact').should('exist');
-      cy.get('#about').should('exist');
-      cy.get('#btnLogout').should('be.visible');
-      cy.get('#admin').should('not.exist');
-    });
-  })  
+describe("User Test Suite", () => {
+  beforeEach(() => {
+    cy.visit("index.html");
+    cy.get("#usermode").click();
+  });
+
+  it("Proper headings and navbar", () => {
+    cy.visit("index.html");
+    cy.get("#email").click().focused().type("test@test.com");
+    cy.get("#password").click().focused().type("test1234");
+    cy.get("#submit").click();
+    cy.get("#header").should("exist");
+    cy.get("#map").should("exist");
+    cy.get('button[class="navbar-icon"]').click();
+    cy.get("#header").should("exist");
+    cy.get("#home").should("exist");
+    cy.get("#edit").should("exist");
+    cy.get("#contact").should("exist");
+    cy.get("#about").should("exist");
+    cy.get("#btnLogout").should("be.visible");
+    cy.get("#admin").should("not.exist");
+  });
+});
